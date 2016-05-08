8348d19e33288df516267f2244cb174fe41e54d4
kevin.lewis@cubic.com <kevin.lewis@cubic.com>
2016-05-07 20:31:17 -0700

    Adding unit test.

---
+++ AssistantJack/edu/miracosta/cs113/hw1/assistant/test/AssistantJackTester.java
@@ -0,0 +1,17 @@
+package edu.miracosta.cs113.hw1.assistant.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import edu.miracosta.cs113.hw1.assistant.AssistantJack;
+
+public class AssistantJackTester {
+
+	@Test
+	public void testCheckAnswerIntIntInt() {
+		AssistantJack aj = new AssistantJack(1);
+		assertTrue(aj.checkAnswer(1, 1, 1) == 0);		
+	}
+
+}
